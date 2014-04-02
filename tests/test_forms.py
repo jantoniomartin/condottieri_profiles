698315bbfd255800bd4a4748a946dc4088bd7fb0
José Antonio Martín <jantonio.martin@gmail.com>
2014-04-02 18:39:34 +0200

    add tests for models and forms

---
+++ tests/test_forms.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+from condottieri_profiles import forms
+
+class ProfileFormTestCase(TestCase):
+	fixtures = []
+
+	def test_profile_form(self):
+		form = forms.ProfileForm(data={
+				'name': 'John Doe',
+				'about': 'I am nobody',
+				'location': 'Nowhere',
+				'website': 'http://example.com',
+				'autosubscribe': True,
+			})
+		self.failUnless(form.is_valid())
+
+
---
+++ tests/test_models.py
@@ -0,0 +1,75 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from condottieri_profiles.models import CondottieriProfile
+from condottieri_profiles.defaults import *
+
+class CondottieriProfileTestCase(TestCase):
+	fixtures = []
+
+	def setUp(self):
+		user_0 = User.objects.create(
+			username="user_0"
+			)
+		user_1 = User.objects.create(
+			username="user_1"
+			)
+
+	def test_get_absolute_url(self):
+		user_0 = User.objects.get(username="user_0")
+		profile = user_0.get_profile()
+		self.assertEqual(profile.get_absolute_url(),
+				'/profiles/profile/user_0')
+
+	def test_has_languages(self):
+		user_0 = User.objects.get(username="user_0")
+		user_1 = User.objects.get(username="user_1")
+		user_0.get_profile().spokenlanguage_set.create(code='en')
+		self.assertTrue(user_0.get_profile().has_languages())
+		self.assertFalse(user_1.get_profile().has_languages())
+
+	def test_average_score(self):
+		user_0 = User.objects.get(username="user_0")
+		prof = user_0.get_profile()
+		self.assertEqual(prof.average_score(), 0)
+		prof.finished_games = 10
+		prof.total_score = 5
+		prof.save()
+		self.assertEqual(prof.average_score(), 0.5)
+
+	def test_adjust_karma(self):
+		user_0 = User.objects.get(username="user_0")
+		prof = user_0.get_profile()
+		karma = prof.karma
+		prof.adjust_karma(1)
+		self.assertEqual(prof.karma, karma + 1)
+		prof.adjust_karma(-2)
+		self.assertEqual(prof.karma, karma - 1)
+		prof.adjust_karma(KARMA_MAXIMUM + 1)
+		self.assertEqual(prof.karma, KARMA_MAXIMUM)
+		prof.adjust_karma(-2 * KARMA_MAXIMUM)
+		self.assertEqual(prof.karma, KARMA_MINIMUM)
+
+	def test_overthrow(self):
+		user_0 = User.objects.get(username="user_0")
+		prof = user_0.get_profile()
+		o = prof.overthrows
+		prof.overthrow()
+		self.assertEqual(prof.overthrows, o + 1)
+
+	def test_check_karma_to_join(self):
+		##TODO
+		pass
+
+	def test_add_overthrow(self):
+		##TODO
+		pass
+
+	def test_add_surrender(self):
+		##TODO
+		pass
+
+	def test_create_profile(self):
+		user_2 = User.objects.create(username="user_2")
+		self.assertIsInstance(user_2.get_profile(), CondottieriProfile)
+
