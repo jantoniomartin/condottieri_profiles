6e6129a9bf563739754f48e775a4411939aa03ca
José Antonio Martín Prieto <jantonio.martin@gmail.com>
2018-02-13 18:42:45 +0100

    added a new SpokenLanguageForm

--- forms.py
+++ forms.py
@@ -1,14 +1,19 @@
 from django.forms import ModelForm
 
-from condottieri_profiles.models import CondottieriProfile
+from condottieri_profiles.models import CondottieriProfile, SpokenLanguage
 
 class ProfileForm(ModelForm):
-	class Meta:
-		model = CondottieriProfile
-		exclude = ('user',
-			'badges',
-			'is_editor',
-			'post_count',
-			'signature',
-			'signature_html',
-			'show_signatures',)
+    class Meta:
+        model = CondottieriProfile
+        exclude = ('user',
+            'badges',
+            'is_editor',
+            'post_count',
+            'signature',
+            'signature_html',
+            'show_signatures',)
+
+class SpokenLanguageForm(ModelForm):
+    class Meta:
+        model = SpokenLanguage
+        fields = ['code',]
--- views.py
+++ views.py
@@ -16,71 +16,72 @@ from condottieri_profiles.models import *
 from condottieri_profiles.forms import *
 
 class LoginRequiredMixin(object):
-	##TODO: move this to middleware
-	@method_decorator(login_required)
-	def dispatch(self, *args, **kwargs):
-		return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
+    ##TODO: move this to middleware
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
 
 class ProfileDetailView(LoginRequiredMixin, DetailView):
-	model = User
-	template_name = 'condottieri_profiles/profile_detail.html'
-	slug_field = 'username'
-	slug_url_kwarg = 'username'
+    model = User
+    template_name = 'condottieri_profiles/profile_detail.html'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
 
-	def get_context_data(self, **kwargs):
-		ctx = super(ProfileDetailView, self).get_context_data(**kwargs)
-		is_friend = am_friend = None
-		ctx.update({
-			'profile': self.object.profile,
-			'friends': self.object.friends.all(),
-			'chosen_by': self.object.friend_of.all(),
-			'is_own': self.request.user == self.object,
-		})
-		if self.request.user != self.object:
-			ctx.update({
-				'is_friend': self.object.id in self.request.user.friends. \
-						values_list('friend_to', flat=True),
-				'am_friend': self.request.user.id in self.object.friend_of. \
-						values_list('friend_to', flat=True),
-			})
-		return ctx
+    def get_context_data(self, **kwargs):
+        ctx = super(ProfileDetailView, self).get_context_data(**kwargs)
+        is_friend = am_friend = None
+        ctx.update({
+            'profile': self.object.profile,
+            'friends': self.object.friends.all(),
+            'chosen_by': self.object.friend_of.all(),
+            'is_own': self.request.user == self.object,
+        })
+        if self.request.user != self.object:
+            ctx.update({
+                'is_friend': self.object.id in self.request.user.friends. \
+                        values_list('friend_to', flat=True),
+                'am_friend': self.request.user.id in self.object.friend_of. \
+                        values_list('friend_to', flat=True),
+            })
+        return ctx
 
 class LanguagesInline(InlineFormSet):
-	model = SpokenLanguage
-	extra = 1
+    model = SpokenLanguage
+    form_class = SpokenLanguageForm
+    extra = 1
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateWithInlinesView):
-	model = CondottieriProfile
-	form_class = ProfileForm
-	template_name = 'condottieri_profiles/profile_form.html'
-	inlines = [LanguagesInline,]
+    model = CondottieriProfile
+    form_class = ProfileForm
+    template_name = 'condottieri_profiles/profile_form.html'
+    inlines = [LanguagesInline,]
 
-	def get_object(self, queryset=None):
-		return self.request.user.profile
+    def get_object(self, queryset=None):
+        return self.request.user.profile
 
-	def get_success_url(self):
-		messages.success(self.request, _("Your profile has been updated."))
-		return self.request.user.profile.get_absolute_url()
+    def get_success_url(self):
+        messages.success(self.request, _("Your profile has been updated."))
+        return self.request.user.profile.get_absolute_url()
 
 class ToggleFriendshipView(LoginRequiredMixin, RedirectView):
-	def get_redirect_url(self, *args, **kwargs):
-		user_from = self.request.user
-		user_to = get_object_or_404(User, username=self.kwargs['username'])
-		try:
-			friendship = Friendship.objects.get(
-				friend_from=user_from,
-				friend_to=user_to
-			)
-		except ObjectDoesNotExist:
-			friendship = Friendship.objects.create(
-				friend_from=user_from,
-				friend_to=user_to
-			)
-			friendship.save()
-			msg = _("%s is now your friend.") % user_to.username
-		else:
-			friendship.delete()
-			msg = _("%s is no longer your friend.") % user_to.username
-		messages.success(self.request, msg)
-		return user_to.profile.get_absolute_url()
+    def get_redirect_url(self, *args, **kwargs):
+        user_from = self.request.user
+        user_to = get_object_or_404(User, username=self.kwargs['username'])
+        try:
+            friendship = Friendship.objects.get(
+                friend_from=user_from,
+                friend_to=user_to
+            )
+        except ObjectDoesNotExist:
+            friendship = Friendship.objects.create(
+                friend_from=user_from,
+                friend_to=user_to
+            )
+            friendship.save()
+            msg = _("%s is now your friend.") % user_to.username
+        else:
+            friendship.delete()
+            msg = _("%s is no longer your friend.") % user_to.username
+        messages.success(self.request, msg)
+        return user_to.profile.get_absolute_url()
 
